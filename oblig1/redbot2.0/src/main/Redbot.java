382691badd295af0ad67deecb6e667de8ece315f
rdamiano <rdamiano@mail.com>
2014-09-16 21:18:28 -0300

    asd

--- oblig1/redbot2.0/src/main/Redbot.java
+++ oblig1/redbot2.0/src/main/Redbot.java
@@ -8,6 +8,7 @@ public class Redbot {
 			struct.inicializarEstructuras(args);	
 			struct.procesarURL(struct.getRaiz(), 0);
 			System.out.println("TERMINE!!!!!!!!!!!!!!");
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
