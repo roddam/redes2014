3ce387dfe9ab8e8cd843e1494dacf523178f7dfa
rdamiano <rdamiano@mail.com>
2014-09-20 11:48:08 -0300

    asdasd

---
+++ oblig1/oblig1/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		Procesamiento q = new Procesamiento();
+		new ProcesamientoChapa(q);
+		new Redbot(q);
+	}
+}
--- oblig1/oblig1/src/main/Redbot.java
+++ oblig1/oblig1/src/main/Redbot.java
@@ -1,41 +1,16 @@
 package main;
 
-public class Redbot {
+public class Redbot implements Runnable {
+	private Procesamiento q;
 
-	public void metodoPrueba() {
-
+	public Redbot(Procesamiento q) {
+		this.q = q;
+		new Thread(this, "Consumer").start();
 	}
 
-	public static void main(String[] args) {
-		Procesamiento procesamiento = new Procesamiento();
-		try {
-			procesamiento.parserEntrada();
-			procesamiento.inicializarRedbot();
-			procesamiento.DFS();
-		} catch (NumberFormatException e) {
-			System.out.println("########## SE ROMPIO #############");
-			e.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("################################# " + e.getMessage());
-			e.printStackTrace();
+	public void run() {
+		for (int i = 0; i<10; i++) {
+			q.get();
 		}
 	}
-
-	/*
-	 * Procedimiento DFS(visitados, camino, profundidad) Comienzo v =
-	 * lista_URLs.primero Si profundidad == 0 //es pozo, por lo que vimos en el
-	 * monitoreo creo que este es el caso de un pozo. //
-	 * "Si parás por profundidad y tenes links de salida son " Else Si
-	 * !visitados.pertenece(v) visitados.agregar(v) camino.agregar(v)
-	 * Preprocesamiento(v,lista_URLs,mails) //en lista_URLs agrego las URLs
-	 * hijas para seguir navegando, mails lo uso para imprimir en pantalla
-	 * Imprimir(mails) //Ver pq no respeto lo de tiempo real Si
-	 * !lista_URLs.vacio() profundidad-- DFS(visitados,camino, profundidad) Else
-	 * //es pozo??? Fin camino.sacar(v) Else Si camino.pertenece(v) ciclos++ Fin
-	 * Fin Fin Fin
-	 * 
-	 * Procedimiento Recorrido_DFS(G: grafo) {G = (V,A)} Var v: vértice Comienzo
-	 * //Lo único es meter la URL original en la lista base Fin
-	 */
-
 }
